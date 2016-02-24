03f2f5473fb639ae9c755499e108e4f9112df8e7
camlang <camlang41@gmail.com>
2016-02-24 23:43:19 +0300

    Initialize Project

---
+++ Sources/UnityBones/Assets/Scripts/Editor/BoneIkEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Assets.Bones.Scripts;
+
+namespace Assets.Bones
+{
+	[CustomEditor(typeof(BoneIK))]
+	public class BoneIkEditor: Editor
+	{
+		private Bone _bone;
+		private BoneIK _boneIk;
+
+		private SerializedProperty IkTarget;
+		private SerializedProperty ShowAngleLimits;
+		private SerializedProperty ChainLength;
+		void OnEnable()
+		{
+			// Setup the SerializedProperties.
+			IkTarget = serializedObject.FindProperty("Target");
+			ShowAngleLimits = serializedObject.FindProperty("ShowAngleLimits");
+			ChainLength = serializedObject.FindProperty("chainLength");
+			_boneIk = (BoneIK)target;
+			_bone = _boneIk.GetComponent<Bone>();
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+
+			EditorGUILayout.PropertyField(ChainLength);
+			EditorGUILayout.PropertyField(IkTarget);
+			EditorGUILayout.PropertyField(ShowAngleLimits);
+
+			EditorGUILayout.BeginHorizontal();
+			if (IkTarget.objectReferenceValue == null && GUILayout.Button(new GUIContent("Create Target")))
+			{
+				BoneIK.CreateTarget();
+			}
+			EditorGUILayout.EndHorizontal();
+			serializedObject.ApplyModifiedProperties();
+
+			DrawDefaultInspector();
+		}
+	}
+}
---
+++ Sources/UnityBones/Assets/Scripts/Skelet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+namespace Assets.Bones.Scripts
+{
+	[ExecuteInEditMode]
+	public class Skelet: MonoBehaviour
+	{
+		void Start()
+		{
+			
+		}
+		void LateUpdate()
+		{
+			if (!Application.isPlaying)
+				Start();
+		}
+
+		void Update()
+		{
+			//EditorUpdate();
+		}
+
+#if UNITY_EDITOR
+		void OnEnable()
+		{
+			//EditorApplication.update += EditorUpdate;
+		}
+
+		void OnDisable()
+		{
+			//EditorApplication.update -= EditorUpdate;
+		}
+#endif
+	}
+}
